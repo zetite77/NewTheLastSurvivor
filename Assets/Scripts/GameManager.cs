abdfcdf77fcef517e481824cbc2f247f1ef7c814
zetite77 <64113427+zetite77@users.noreply.github.com>
2022-01-20 13:57:32 +0900

    낮게이지 작성에 따른 각종 버그 수정
    1. GameManager 에서 onPlayCanvas로 게임플레이적인 코드 전부 이동 (타이밍적인 민감한 코드 싹다 개선)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,12 +23,12 @@ public class GameManager : MonoBehaviour
     }
     #endregion
 
-    public Image m_ProgBar_NightTime;
-    public Text m_ProgBar_TestTxt;
-    public Text m_Text_StageTxt;
-    public Image m_ProgBar_DayTime;
+    //public Image m_ProgBar_NightTime;
+    //public Text m_ProgBar_TestTxt;
+    //public Text m_Text_StageTxt;
+    //public Image m_ProgBar_DayTime;
 
-    public GameObject m_objOnPlayCanvas; //수정(김상현)22.01.17 원코드 : private Canvas = Cvs_OnPlayCanvas;
+    //public GameObject m_objOnPlayCanvas; //수정(김상현)22.01.17 원코드 : private Canvas = Cvs_OnPlayCanvas;
     public GameObject m_objTitleCanvas;
     public GameObject m_objUpgradeCanvas;
     public GameObject m_objSetupCanvas;
@@ -37,12 +37,12 @@ public class GameManager : MonoBehaviour
     public GameObject m_objGrenadeOffBtn;
 
     // 스테이지 컨트롤
-    private bool dayNightFlg = false; // 낮 = true, 밤 = false
-    public float stageDuration; // 낮/밤 지속시간(단위:초)
-    private int numberOfTime; // 몇 초 지났는지 int형으로 저장.
-    public int numberOfStage = 1; // 스테이지 단계(항상 1단계부터 시작)
-    const float PROGRESS_MAX = 1.0f; // 프로그래스 바 게이지 최대치(100%)
-    const float PROGRESS_MIN = 0.0f;
+    //private bool dayNightFlg = false; // 낮 = true, 밤 = false
+    //public float stageDuration; // 낮/밤 지속시간(단위:초)
+    //private int numberOfTime; // 몇 초 지났는지 int형으로 저장.
+    //public int numberOfStage = 1; // 스테이지 단계(항상 1단계부터 시작)
+    //const float PROGRESS_MAX = 1.0f; // 프로그래스 바 게이지 최대치(100%)
+    //const float PROGRESS_MIN = 0.0f;
 
     // 오디오
     public Button Btn_Next;
@@ -53,22 +53,18 @@ public class GameManager : MonoBehaviour
     int soundTrackNum = 0;
     int preTrackNum = 0;
 
-    public int m_nGrenadecount = 3;
+    //public int m_nGrenadecount = 3;
 
     void Start()
     {
-        Canvas Cvs_OnPlayCanvas = m_objOnPlayCanvas.GetComponent<Canvas>();       // 수정(김상현)22.01.17 원코드 : Cvs_OnPlayCanvas = GetComponent<Canvas>();
-        m_ProgBar_NightTime.fillAmount = PROGRESS_MAX;
+        //Canvas Cvs_OnPlayCanvas = m_objOnPlayCanvas.GetComponent<Canvas>();       // 수정(김상현)22.01.17 원코드 : Cvs_OnPlayCanvas = GetComponent<Canvas>();
+        //m_ProgBar_NightTime.fillAmount = PROGRESS_MAX;
 
         // 초기 볼륨 50%설정
         const float INIT_VOLUME = 0.5f;
         foreach (AudioSource soundEffect in m_NightSound)
             soundEffect.volume = INIT_VOLUME;
         m_BackgroundMusic.volume = INIT_VOLUME;
-
-        GameStartButton.onClick.AddListener(InitStage);
-        GameStartButton2.onClick.AddListener(InitStage);
-        Btn_Next.onClick.AddListener(InitNightProcess); 
 
         // 스테이지 시작 시 사운드트랙 재생 (InitNightProcess로 대체됨)
         //Btn_Next.onClick.AddListener(SoundTrackPlay); 
@@ -78,90 +74,99 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        if (dayNightFlg) DayProcess();
-        else NightProcess();
+        //if (dayNightFlg) DayProcess();
+       // else NightProcess();
     }
 
-    void DayProcess()
-    {
-        if (m_ProgBar_NightTime.fillAmount < PROGRESS_MAX)
-        { // 초당 1 / duration 만큼 부드럽게(매 프레임 마다) 증가 
-            // duration이 15일 경우,         0초 일 때 1  ->  7.5초 일 때 0.5  ->  15초 일 때 0
-            m_ProgBar_NightTime.fillAmount += PROGRESS_MAX / stageDuration * Time.deltaTime;
-
-            #region 테스트用임. 나중에 지울것(변상현)
-            // Math.Ceiling = double형 실수를 정수부분까지 올림
-            numberOfTime = (int)System.Math.Ceiling(stageDuration - m_ProgBar_NightTime.fillAmount * stageDuration);
-            m_ProgBar_TestTxt.text = numberOfTime + "s";
-            #endregion
-        }
-        else
-        { // 게이지가 모두 소진되면 밤 프로세스 시작
-            InitNightProcess();
-        }
+    public void ChangeMusic()
+    { // 스테이지 시작 효과음 재생 및 변경
+        m_NightSound[preTrackNum].enabled = false;
+        m_NightSound[soundTrackNum].enabled = true;
+        preTrackNum = soundTrackNum;
+        soundTrackNum = (soundTrackNum < m_NightSound.Length - 1) ? (soundTrackNum + 1) : 0;
     }
 
-    void InitStage()
-    { // 스테이지 초기화 (게임을 아예 처음부터 시작하는 경우)
-        numberOfStage = 0; // initNight하면서 1더함ㄱㅊ
-        InitNightProcess();
-    }
+    //void DayProcess()
+    //{
+    //    if (m_ProgBar_NightTime.fillAmount < PROGRESS_MAX)
+    //    { // 초당 1 / duration 만큼 부드럽게(매 프레임 마다) 증가 
+    //        // duration이 15일 경우,         0초 일 때 1  ->  7.5초 일 때 0.5  ->  15초 일 때 0
+    //        m_ProgBar_NightTime.fillAmount += PROGRESS_MAX / stageDuration * Time.deltaTime;
 
-    void InitNightProcess()
-    {
-        dayNightFlg = false;
-        m_objUpgradeCanvas.SetActive(false);
-        m_ProgBar_NightTime.fillAmount = PROGRESS_MAX;
+    //        #region 테스트用임. 나중에 지울것(변상현)
+    //        // Math.Ceiling = double형 실수를 정수부분까지 올림
+    //        numberOfTime = (int)System.Math.Ceiling(stageDuration - m_ProgBar_NightTime.fillAmount * stageDuration);
+    //        m_ProgBar_TestTxt.text = numberOfTime + "s";
+    //        #endregion
+    //    }
+    //    else
+    //    { // 게이지가 모두 소진되면 밤 프로세스 시작
+    //        InitNightProcess();
+    //    }
+    //}
 
-        m_ProgBar_TestTxt.text = stageDuration + "s"; // 테스트用임. 나중에 지울것(변상현)
-        numberOfStage++;
-        m_Text_StageTxt.text = "Stage" + numberOfStage;
+    //void InitStage()
+    //{ // 스테이지 초기화 (게임을 아예 처음부터 시작하는 경우)
+    //    numberOfStage = 0; // initNight하면서 1더함ㄱㅊ
+    //    InitNightProcess();
+    //}
 
-        { // 스테이지 시작 효과음 재생 및 변경
-            m_NightSound[preTrackNum].enabled = false;
-            m_NightSound[soundTrackNum].enabled = true;
-            preTrackNum = soundTrackNum;
-            soundTrackNum = (soundTrackNum < m_NightSound.Length - 1) ? (soundTrackNum + 1) : 0;
-        }
-    }
+    //void InitNightProcess()
+    //{
+    //    dayNightFlg = false;
+    //    m_objUpgradeCanvas.SetActive(false);
+    //    m_ProgBar_NightTime.fillAmount = PROGRESS_MAX;
 
-    void NightProcess()
-    {
-        if (m_ProgBar_NightTime.fillAmount > PROGRESS_MIN)
-        { // 초당 1 / duration 만큼 부드럽게(매 프레임 마다) 감소 
-            // duration이 15일 경우,         0초 일 때 1  ->  7.5초 일 때 0.5  ->  15초 일 때 0
-            m_ProgBar_NightTime.fillAmount -= PROGRESS_MAX / stageDuration * Time.deltaTime;
+    //    m_ProgBar_TestTxt.text = stageDuration + "s"; // 테스트用임. 나중에 지울것(변상현)
+    //    numberOfStage++;
+    //    m_Text_StageTxt.text = "Stage" + numberOfStage;
 
-            #region 테스트用임. 나중에 지울것(변상현)
-            // Math.Ceiling = double형 실수를 정수부분까지 올림
-            numberOfTime = (int)System.Math.Ceiling(m_ProgBar_NightTime.fillAmount * stageDuration);
-            m_ProgBar_TestTxt.text = numberOfTime + "s";
-            #endregion
-        }
-        else
-        { // 게이지가 모두 소진되면 낮 프로세스 시작
-            m_objUpgradeCanvas.SetActive(true);
-            m_objGrenade.GetComponent<Grenade>().SetCount(3);
-            m_objGrenadeOnBtn.SetActive(true);
-            m_objGrenadeOffBtn.SetActive(false);
+    //    { // 스테이지 시작 효과음 재생 및 변경
+    //        m_NightSound[preTrackNum].enabled = false;
+    //        m_NightSound[soundTrackNum].enabled = true;
+    //        preTrackNum = soundTrackNum;
+    //        soundTrackNum = (soundTrackNum < m_NightSound.Length - 1) ? (soundTrackNum + 1) : 0;
+    //    }
+    //}
 
-            dayNightFlg = true;
-            m_ProgBar_TestTxt.text = stageDuration + "s"; // 테스트用임. 나중에 지울것(변상현)
-        }
+
+    //void NightProcess()
+    //{
+    //    if (m_ProgBar_NightTime.fillAmount > PROGRESS_MIN)
+    //    { // 초당 1 / duration 만큼 부드럽게(매 프레임 마다) 감소 
+    //        // duration이 15일 경우,         0초 일 때 1  ->  7.5초 일 때 0.5  ->  15초 일 때 0
+    //        m_ProgBar_NightTime.fillAmount -= PROGRESS_MAX / stageDuration * Time.deltaTime;
+
+    //        #region 테스트用임. 나중에 지울것(변상현)
+    //        // Math.Ceiling = double형 실수를 정수부분까지 올림
+    //        numberOfTime = (int)System.Math.Ceiling(m_ProgBar_NightTime.fillAmount * stageDuration);
+    //        m_ProgBar_TestTxt.text = numberOfTime + "s";
+    //        #endregion
+    //    }
+    //    else
+    //    { // 게이지가 모두 소진되면 낮 프로세스 시작
+    //        m_objUpgradeCanvas.SetActive(true);
+    //        m_objGrenade.GetComponent<Grenade>().SetCount(3);
+    //        m_objGrenadeOnBtn.SetActive(true);
+    //        m_objGrenadeOffBtn.SetActive(false);
+
+    //        dayNightFlg = true;
+    //        m_ProgBar_TestTxt.text = stageDuration + "s"; // 테스트用임. 나중에 지울것(변상현)
+    //    }
         
-        //OnPlayCanvas_EnableChanged();
-        SetGranade();
-    }
+    //    //OnPlayCanvas_EnableChanged();
+    //    SetGranade();
+    //}
 
-    public void SetGranade()
-    {
-        m_nGrenadecount = m_objGrenade.GetComponent<Grenade>().GetCount();
-        if (m_nGrenadecount <= 0)
-        {
-            m_objGrenadeOnBtn.SetActive(false);
-            m_objGrenadeOffBtn.SetActive(true);
-        }
-    }
+    //public void SetGranade()
+    //{
+    //    m_nGrenadecount = m_objGrenade.GetComponent<Grenade>().GetCount();
+    //    if (m_nGrenadecount <= 0)
+    //    {
+    //        m_objGrenadeOnBtn.SetActive(false);
+    //        m_objGrenadeOffBtn.SetActive(true);
+    //    }
+    //}
 
 
     // DayProcess()로 대체됨.
---
+++ Assets/Scripts/OnPlayScript.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnPlayScript : MonoBehaviour
+{
+    public Image m_ProgBar_NightTime;
+    public Text m_ProgBar_TestTxt;
+    public Text m_Text_StageTxt;
+
+    public GameObject m_objUpgradeCanvas;
+    public GameObject m_objGrenade;
+    public GameObject m_objGrenadeOnBtn;
+    public GameObject m_objGrenadeOffBtn;
+
+    // 스테이지 컨트롤
+    private bool dayNightFlg = false; // 낮 = true, 밤 = false
+    public float stageDuration; // 낮/밤 지속시간(단위:초)
+    private int numberOfTime; // 몇 초 지났는지 int형으로 저장.
+    public int numberOfStage = 1; // 스테이지 단계(항상 1단계부터 시작)
+    const float PROGRESS_MAX = 1.0f; // 프로그래스 바 게이지 최대치(100%)
+    const float PROGRESS_MIN = 0.0f;
+
+    // 플레이 관련
+    public int m_nGrenadecount = 3;
+
+    private void Start()
+    {
+        m_ProgBar_NightTime.fillAmount = PROGRESS_MAX;
+    }
+
+    void Update()
+    {
+        if (dayNightFlg) DayProcess();
+        else NightProcess();
+    }
+
+    void DayProcess()
+    {
+        if (m_ProgBar_NightTime.fillAmount < PROGRESS_MAX)
+        { // 초당 1 / duration 만큼 부드럽게(매 프레임 마다) 증가 
+            // duration이 15일 경우,         0초 일 때 1  ->  7.5초 일 때 0.5  ->  15초 일 때 0
+            m_ProgBar_NightTime.fillAmount += PROGRESS_MAX / stageDuration * Time.deltaTime;
+
+            #region 테스트用임. 나중에 지울것(변상현)
+            // Math.Ceiling = double형 실수를 정수부분까지 올림
+            numberOfTime = (int)System.Math.Ceiling(stageDuration - m_ProgBar_NightTime.fillAmount * stageDuration);
+            m_ProgBar_TestTxt.text = numberOfTime + "s";
+            #endregion
+        }
+        else
+        { // 게이지가 모두 소진되면 밤 프로세스 시작
+            InitNightProcess();
+        }
+    }
+
+    public void InitStage()
+    { // 스테이지 초기화 (게임을 아예 처음부터 시작하는 경우)
+        numberOfStage = 0; // initNight하면서 1더함ㄱㅊ
+        InitNightProcess();
+    }
+
+    public void InitNightProcess()
+    {
+        dayNightFlg = false;
+        m_objUpgradeCanvas.SetActive(false);
+        m_ProgBar_NightTime.fillAmount = PROGRESS_MAX;
+
+        m_ProgBar_TestTxt.text = stageDuration + "s"; // 테스트用임. 나중에 지울것(변상현)
+        numberOfStage++;
+        m_Text_StageTxt.text = "Stage" + numberOfStage;
+
+        GameManager.Instance.ChangeMusic();
+    }
+
+    void NightProcess()
+    {
+        if (m_ProgBar_NightTime.fillAmount > PROGRESS_MIN)
+        { // 초당 1 / duration 만큼 부드럽게(매 프레임 마다) 감소 
+            // duration이 15일 경우,         0초 일 때 1  ->  7.5초 일 때 0.5  ->  15초 일 때 0
+            m_ProgBar_NightTime.fillAmount -= PROGRESS_MAX / stageDuration * Time.deltaTime;
+
+            #region 테스트用임. 나중에 지울것(변상현)
+            // Math.Ceiling = double형 실수를 정수부분까지 올림
+            numberOfTime = (int)System.Math.Ceiling(m_ProgBar_NightTime.fillAmount * stageDuration);
+            m_ProgBar_TestTxt.text = numberOfTime + "s";
+            #endregion
+        }
+        else
+        { // 게이지가 모두 소진되면 낮 프로세스 시작
+            m_objUpgradeCanvas.SetActive(true);
+            m_objGrenade.GetComponent<Grenade>().SetCount(3);
+            m_objGrenadeOnBtn.SetActive(true);
+            m_objGrenadeOffBtn.SetActive(false);
+
+            dayNightFlg = true;
+            m_ProgBar_TestTxt.text = stageDuration + "s"; // 테스트用임. 나중에 지울것(변상현)
+        }
+
+        //OnPlayCanvas_EnableChanged();
+        SetGranade();
+    }
+
+    public void SetGranade()
+    {
+        m_nGrenadecount = m_objGrenade.GetComponent<Grenade>().GetCount();
+        if (m_nGrenadecount <= 0)
+        {
+            m_objGrenadeOnBtn.SetActive(false);
+            m_objGrenadeOffBtn.SetActive(true);
+        }
+    }
+}
